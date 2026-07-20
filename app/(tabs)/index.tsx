ea4eeb04601bc77921678f6cf627524f71652d3c
lixin <lixinloh86@gmail.com>
2026-07-20 21:07:27 +0100

    I have changed the wording from Hello! to something else

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello!</ThemedText>
+        <ThemedText type="title">I made this Gogo!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
